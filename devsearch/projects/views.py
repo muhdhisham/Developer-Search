06f4eb78fa7772fb27bcda6773885a9249e26577
muhdhisham <muhdhisham034@gmail.com>
2022-11-29 21:08:45 +0530

    pagination added

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -4,14 +4,15 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from .models import Project, Tag
 from .forms import ProjectForm
-from .utils import searchProjects
+from .utils import searchProjects, paginateProjects
 # Create your views here.
 
 
 
 def projects(request):
     projects, search_query =  searchProjects(request)
-    context = {'projects':projects, 'search_query':search_query}
+    custom_range, projects = paginateProjects(request, projects, 2)
+    context = {'projects':projects, 'search_query':search_query,'custom_range':custom_range}
     return render(request, 'projects/projects.html', context)
     
     
